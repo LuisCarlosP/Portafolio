9cec0456660419a53284a8157fe8cf80ad7c82c2
LuisCarlosPicadoRojas <144180800+LuisCarlosPicadoRojas@users.noreply.github.com>
2024-06-02 21:02:34 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+ 
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu');
+        toggle.classList.toggle('show-icon');
+    });
+
+    const dropdownLinks = document.querySelectorAll('.dropdown__menu .dropdown__link');
+    dropdownLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            const language = link.getAttribute('data-language');
+            changeLanguage(language);
+        });
+    });
+}
+
+const currentLanguage = "es"; 
+changeLanguage(currentLanguage);
+
+function changeLanguage(language) {
+    fetch(`languages/${language}.json`)
+        .then(response => response.json())
+        .then(data => {
+            const elementsWithTranslation = document.querySelectorAll('[data-i18n]');
+            elementsWithTranslation.forEach(element => {
+                const key = element.getAttribute('data-i18n');
+                element.textContent = data[key];
+            });
+        })
+        .catch(error => {
+            console.error("Error loading translation file:", error);
+        });
+}
+
+showMenu('nav-toggle','nav-menu');
